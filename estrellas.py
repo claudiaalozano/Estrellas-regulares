be06383654589529d98cac9b201a4ed98f016d52
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-20 20:08:01 +0100

    creo condicionales para las puntas de las estrellas

--- estrellas.py
+++ estrellas.py
@@ -3,3 +3,8 @@ puntas= int(input("¿Cuantas puntas quieres que tenga la estrella?:"))
 
 def calculo(puntas):
     angulo=0
+    if puntas%2 == 0:
+        angulo= 360 - (180/puntas)
+    if puntas% 3 == 0:
+        angulo= 120 + (180/puntas)
+    
