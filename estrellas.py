3ef3fa9a7c37a4bbf47e0c807407eb0599d65015
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-20 21:01:25 +0100

    libreria turtle

--- estrellas.py
+++ estrellas.py
@@ -1,10 +1,14 @@
 import turtle
-puntas= int(input("¿Cuantas puntas quieres que tenga la estrella?:"))
 
+puntas= int(input("¿Cuantas puntas quieres que tenga la estrella?:"))
+ws= turtle.Screen()
+geekyTurtle= turtle.Turtle()
 def calculo(puntas):
     angulo=0
     if puntas%2 == 0:
         angulo= 360 - (180/puntas)
     if puntas% 3 == 0:
         angulo= 120 + (180/puntas)
-    
+    else:
+        pass
+
