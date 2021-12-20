59c2b2e42c40c9dc2ef41bf2058336547a35b9c7
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-20 21:06:07 +0100

    fallo con las definiciones

--- estrellas.py
+++ estrellas.py
@@ -1,14 +1,19 @@
 import turtle
-
-puntas= int(input("¿Cuantas puntas quieres que tenga la estrella?:"))
 ws= turtle.Screen()
 geekyTurtle= turtle.Turtle()
-def calculo(puntas):
-    angulo=0
-    if puntas%2 == 0:
-        angulo= 360 - (180/puntas)
-    if puntas% 3 == 0:
-        angulo= 120 + (180/puntas)
-    else:
-        pass
 
+
+
+def generar_estrella(puntas):
+    def calculo(puntas):
+        angulo=0
+        if puntas%2 == 0:
+             angulo= 360 - (180/puntas)
+        if puntas% 3 == 0:
+            angulo= 120 + (180/puntas)
+        else:
+            pass
+        return angulo
+
+puntas= int(input("¿Cuántas puntas quieres que tenga la estrella?:"))
+generar_estrella(puntas)
